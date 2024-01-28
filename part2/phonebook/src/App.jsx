4a63edbf464ddf41a1f7a8116bddef5d5e153c4e
neptunejing <jingnanqing@hotmail.com>
2024-01-28 20:31:57 +0200

    part2: exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456' },
-		{ name: 'Ada Lovelace', number: '39-44-5323523' },
-		{ name: 'Dan Abramov', number: '12-43-234345' },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122' },
-	]);
+	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState('');
 	const [newNumber, setNewNumber] = useState('');
 	const [filterString, setFilterString] = useState('');
@@ -33,10 +29,16 @@ const App = () => {
 			alert(`${newName} is already added to phonebook`);
 			return;
 		}
-		setPersons(persons.concat({ name: newName, number: newNumber }));
+		setPersons(persons.concat({ name: newName, number: newNumber, idx: persons.length + 1}));
 		setNewName('');
 		setNewNumber('');
 	};
+
+	useEffect(() => {
+		axios.get('http://localhost:3000/persons').then((response) => {
+			setPersons(response.data);
+		});
+	}, []);
 
 	return (
 		<div>
