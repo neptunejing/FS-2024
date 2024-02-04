1ad5dbc58803c73001bdf39c9136c04c1c2b0c2d
neptunejing <jingnanqing@hotmail.com>
2024-02-04 12:29:12 +0200

    part3: exercise 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -50,6 +50,17 @@ app.get('/info', (req, res) => {
 	res.send(`Phonebook has info for ${cnt} people<p>${generateDate()}</p>`);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id);
+	const person = persons.find((person) => person.id === id);
+
+	if (person) {
+		res.json(person);
+	} else {
+		res.status(404).end();
+	}
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
