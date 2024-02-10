d29f7b558952a2137c47737ee83180d11ebcade5
neptunejing <jingnanqing@hotmail.com>
2024-02-10 15:27:08 +0200

    exercise 4.3-4.7

---
+++ part4/blog-backend/utils/list_helper.js
@@ -0,0 +1,78 @@
+const dummy = (blogs) => {
+	return 1;
+};
+
+const totalLikes = (blogs) => {
+	return blogs.reduce((acc, ojb) => acc + ojb.likes, 0);
+};
+
+const mostLikes = (blogs) => {
+	let currMaxLikes = 0;
+	let mostLiked = {};
+	blogs.forEach((blog) => {
+		if (blog.likes > currMaxLikes) {
+			currMaxLikes = blog.likes;
+			mostLiked = blog;
+		}
+	});
+	console.log(mostLiked);
+	return {
+		title: mostLiked.title,
+		author: mostLiked.author,
+		likes: mostLiked.likes,
+	};
+};
+
+const authorWithMostBlogs = (blogs) => {
+	// <author, cntOfBlogs>
+	const map = new Map();
+	blogs.forEach(({ author }) => {
+		if (!map.has(author)) {
+			map.set(author, 0);
+		}
+		map.set(author, map.get(author) + 1);
+	});
+	let author = '';
+	let currMaxCnt = 0;
+	map.forEach((v, k) => {
+		if (v > currMaxCnt) {
+			currMaxCnt = v;
+			author = k;
+		}
+	});
+	return {
+		author: author,
+		blogs: currMaxCnt,
+	};
+};
+
+const authorWithMostLikes = (blogs) => {
+	// <author, cntOfLikes>
+	const map = new Map();
+	blogs.forEach(({ author, likes }) => {
+		if (!map.has(author)) {
+			map.set(author, 0);
+		}
+		map.set(author, map.get(author) + likes);
+	});
+	let author = '';
+	let currMaxCnt = 0;
+	map.forEach((v, k) => {
+		if (v > currMaxCnt) {
+			currMaxCnt = v;
+			author = k;
+		}
+	});
+	return {
+		author: author,
+		likes: currMaxCnt,
+	};
+};
+
+module.exports = {
+	dummy,
+	totalLikes,
+	mostLikes,
+	authorWithMostBlogs,
+	authorWithMostLikes,
+};
