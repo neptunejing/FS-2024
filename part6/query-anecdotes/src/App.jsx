35aaa58d4f406e0b11b78c7aa6c988b91f1eca69
neptunejing <jingnanqing@hotmail.com>
2024-02-17 13:40:23 +0200

    exercise 6.20-6.22

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,71 @@
+import AnecdoteForm from './components/AnecdoteForm';
+import Notification from './components/Notification';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { getAnecdotes, updateVote } from './services/anecdotes';
+
+const App = () => {
+	const queryClient = useQueryClient();
+
+	const handleVote = (anecdote) => {
+		console.log('vote');
+		updateVoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
+	};
+
+	const updateVoteMutation = useMutation(updateVote, {
+		onSuccess: (updatedAnecdote) => {
+			console.log('update', updatedAnecdote);
+
+			const anecdotes = queryClient.getQueryData('anecdotes');
+
+			queryClient.setQueryData(
+				'anecdotes',
+				anecdotes.map((anecdote) =>
+					anecdote.id === updatedAnecdote.id
+						? updatedAnecdote
+						: anecdote
+				)
+			);
+		},
+	});
+
+	const result = useQuery('anecdotes', getAnecdotes, {
+		retry: 1,
+		refetchOnWindowFocus: false,
+	});
+	console.log(result);
+
+	if (result.isLoading) {
+		return <div>loading data...</div>;
+	}
+
+	if (result.isError) {
+		return (
+			<div>anecdote service not avaiable due to problems in server</div>
+		);
+	}
+
+	const anecdotes = result.data;
+
+	return (
+		<div>
+			<h3>Anecdote app</h3>
+
+			<Notification />
+			<AnecdoteForm />
+
+			{anecdotes.map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => handleVote(anecdote)}>
+							vote
+						</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default App;
---
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,38 @@
+import { useQueryClient, useMutation } from 'react-query';
+import { createAnecdote } from '../services/anecdotes';
+
+const AnecdoteForm = () => {
+	const queryClient = useQueryClient();
+
+	const newAnecdoteMutation = useMutation(createAnecdote, {
+		onSuccess: (newAnecdote) => {
+			const anecdotes = queryClient.getQueryData('anecdotes');
+			queryClient.setQueryData(
+				'anecdotes',
+				anecdotes.concat(newAnecdote)
+			);
+		},
+	});
+
+	const getId = () => (100000 * Math.random()).toFixed(0);
+
+	const onCreate = async (event) => {
+		event.preventDefault();
+		const content = event.target.anecdote.value;
+		event.target.anecdote.value = '';
+		newAnecdoteMutation.mutate({ content, id: getId(), votes: 0 });
+		console.log('new anecdote');
+	};
+
+	return (
+		<div>
+			<h3>create new</h3>
+			<form onSubmit={onCreate}>
+				<input name='anecdote' />
+				<button type='submit'>create</button>
+			</form>
+		</div>
+	);
+};
+
+export default AnecdoteForm;
